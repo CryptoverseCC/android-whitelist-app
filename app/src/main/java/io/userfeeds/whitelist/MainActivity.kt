632e9b244e7ce79e83fd34298823f99c6bdc9786
Maciej Górski <maciek.gorski@gmail.com>
2017-07-28 10:41:06 +0200

    Empty app.

---
+++ app/src/main/java/io/userfeeds/whitelist/MainActivity.kt
@@ -0,0 +1,12 @@
+package io.userfeeds.whitelist
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
